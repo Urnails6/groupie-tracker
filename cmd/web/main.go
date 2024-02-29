7b2d8492333cc0524eefc1e4965ea3b6be855027
aramadha <abraraliramadhan@gmail.com>
2024-02-29 13:34:15 +0300

    add navbar & footer & css

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,8 +10,15 @@ import (
 const portNumber = ":8090"
 
 func main() {
+	// Serve static files from the "static" directory
+	FileServer := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", FileServer))
+
+	// Handle routes
 	http.HandleFunc("/", handlers.Home)
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 	http.HandleFunc("/details", handlers.ArtistDetails)
+
+	// Start the server
 	_ = http.ListenAndServe(portNumber, nil)
 }
